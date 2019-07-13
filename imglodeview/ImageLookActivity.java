d6bc26581c816989431ea3e8ea562251f5d7417e
xinnuo <416143467@qq.com>
2019-07-13 11:38:26 +0800

    画廊使用

---
+++ imglodeview/ImageLookActivity.java
@@ -0,0 +1,21 @@
+package com.meida.shaokaoshop.imglodeview;
+
+import com.meida.shaokaoshop.R;
+import com.previewlibrary.GPreviewActivity;
+
+/**
+ * 项目名称：ShaoKaoShop
+ * 创建人：刘
+ * 创建时间：2019-07-13 09:52
+ * 功能描述：
+ */
+public class ImageLookActivity extends GPreviewActivity {
+    /***
+     * 重写该方法
+     * 使用你的自定义布局
+     **/
+    @Override
+    public int setContentLayout() {
+        return R.layout.activity_image_look;
+    }
+}
